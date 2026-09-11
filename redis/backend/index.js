3c452e64bea2be2fe58d136e251ec4af45103868
Muhammad Farooq <fm6182218@gmail.com>
2026-09-11 22:35:18 +0500

    Redis Basic

---
+++ redis/backend/index.js
@@ -0,0 +1,44 @@
+import express from "express";
+import dotenv from "dotenv";
+import { redis } from "./src/config/redis.js";
+import { connectDB } from "./src/config/db.js";
+
+dotenv.config()
+
+const app = express();
+app.use(express.json());
+
+const KEY_REDIS = "value:all"
+
+
+app.post("/post-redis", async (req, res) => {
+    const user = await redis.set("value:all", req.body);
+
+    res.json({
+        message: "Successfully Done",
+        user,
+    });
+});
+
+app.get("/get-redis", async (req, res) => {
+    const user = await redis.get("value:all")
+    res.json({
+        message: "Successfully Done",
+        user,
+    });
+
+})
+
+app.get("/ping", async (req, res) => {
+    const result = await redis.expire(KEY_REDIS, 60);
+
+    res.json({
+        message: "Pong Done",
+        redis: result
+    });
+})
+
+connectDB()
+app.listen(3000, () => {
+    console.log(`server Connected on Port 3000`);
+})
