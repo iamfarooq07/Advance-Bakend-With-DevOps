23ff26751015ab01bc8b5c0eac55bdf98b5b68d1
Muhammad Farooq <fm6182218@gmail.com>
2026-09-13 16:35:06 +0500

    Practies redis Otp Logic [Create, Verify] and etc

--- redis/backend/app.js
+++ redis/backend/app.js
@@ -1,14 +1,12 @@
 import express from "express";
-import mongoose from "mongoose";
 import dotenv from "dotenv";
-import { route } from "./src/routes/redis.route.js";
 import { connectDB } from "./src/config/db.js";
 import { redis } from "./src/config/redis.js"; // Route ya controller me use karne ke liye import kar sakte hain
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 5000;
 
 // Body Parser Middleware
 app.use(express.json());
@@ -18,7 +16,45 @@ app.get("/", (req, res) => {
     res.send("Hello World");
 });
 
-app.use(route);
+// app.use(route);
+
+app.post("/create", async (req, res) => {
+    const { email } = req.body;
+
+    const otp = Math.floor(100000 + Math.random() * 900000);
+
+    const key = `otp:${email}`;
+
+    const sendOtp = await redis.set(key, otp, "EX", 60);
+
+    return res.status(200).json({ message: "OTP sent successfully", otp, sendOtp });
+});
+
+app.post("/verify", async (req, res) => {
+    const { email, otp } = req.body;
+
+    const key = `otp:${email}`;
+
+    const cachedOtp = await redis.get(key);
+
+    if (!cachedOtp) {
+        return res.status(409).json({
+            message: "OTP Not Found"
+        });
+    }
+
+    if (cachedOtp !== String(otp)) {
+        return res.status(401).json({
+            message: "OTP Not Match"
+        });
+    }
+
+    await redis.del(key);
+
+    return res.status(200).json({
+        message: "Verification successful"
+    });
+});
 
 // Start Server
 app.listen(PORT, async () => {
--- redis/backend/index.js
+++ redis/backend/index.js
@@ -1,44 +1,44 @@
-import express from "express";
-import dotenv from "dotenv";
-import { redis } from "./src/config/redis.js";
-import { connectDB } from "./src/config/db.js";
+// import express from "express";
+// import dotenv from "dotenv";
+// import { redis } from "./src/config/redis.js";
+// import { connectDB } from "./src/config/db.js";
 
-dotenv.config()
+// dotenv.config()
 
-const app = express();
-app.use(express.json());
+// const app = express();
+// app.use(express.json());
 
-const KEY_REDIS = "value:all"
+// const KEY_REDIS = "value:all"
 
 
-app.post("/post-redis", async (req, res) => {
-    const user = await redis.set("value:all", req.body);
+// app.post("/post-redis", async (req, res) => {
+//     const user = await redis.set("value:all", req.body);
 
-    res.json({
-        message: "Successfully Done",
-        user,
-    });
-});
+//     res.json({
+//         message: "Successfully Done",
+//         user,
+//     });
+// });
 
-app.get("/get-redis", async (req, res) => {
-    const user = await redis.get("value:all")
-    res.json({
-        message: "Successfully Done",
-        user,
-    });
+// app.get("/get-redis", async (req, res) => {
+//     const user = await redis.get("value:all")
+//     res.json({
+//         message: "Successfully Done",
+//         user,
+//     });
 
-})
+// })
 
-app.get("/ping", async (req, res) => {
-    const result = await redis.expire(KEY_REDIS, 60);
+// app.get("/ping", async (req, res) => {
+//     const result = await redis.expire(KEY_REDIS, 60);
 
-    res.json({
-        message: "Pong Done",
-        redis: result
-    });
-})
+//     res.json({
+//         message: "Pong Done",
+//         redis: result
+//     });
+// })
 
-connectDB()
-app.listen(3000, () => {
-    console.log(`server Connected on Port 3000`);
-})
+// connectDB()
+// app.listen(3001, () => {
+//     console.log(`server Connected on Port 3000`);
+// })
